bb4ff499828904a626934ce1c3cb021e627cabc8
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-01-21 09:36:46 +0530

    Home Page Code Comment

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,12 @@ export default function Home() {
         <div className="container-fluid">
           <a className="navbar-brand align-items-center" href="/">
             <span style={{ color: "brown" }}>
+
+
+              {/*-------------Employee Manegment Logo/Heading--------- */}
+
+
+
               Employee<span style={{ color: "green" }}>Management</span>
             </span>
           </a>
@@ -20,12 +26,23 @@ export default function Home() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+
+
+          {/* ------------------Off Canvanus Bootstarp 5.3 START--------------------- */}
+
+
+
           <div
             className="offcanvas offcanvas-start text-bg-dark show"
             tabIndex="-1"
             id="offcanvasDarkNavbar"
             aria-labelledby="offcanvasDarkNavbarLabel"
           >
+
+            {/* ---------------------Administration Panel ----------------------- */}
+
+
+
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
                 <svg
@@ -44,6 +61,11 @@ export default function Home() {
                 </svg>
                 Administrator
               </h5>
+
+
+              {/* ----------------Search bar-------------- */}
+
+
               <hr style={{ background: "white" }} />
               <button
                 type="button"
@@ -72,7 +94,18 @@ export default function Home() {
                 </svg>
               </button>
             </form>
+
+
+            {/* -------------Main Navigation START--------- */}
+
+
             <div className="container p-2 box1">MAIN NAVIGATION</div>
+
+
+            {/* ----------------------DashBoard-------------------------- */}
+
+
+
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
                 <li className="nav-item">
@@ -94,10 +127,16 @@ export default function Home() {
                         fillRule="evenodd"
                         d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A8 8 0 0 1 0 10m8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3"
                       /></svg>
-                        Dashboard
-                    
+                    Dashboard
+
                   </a>
                 </li>
+                {/* --------------------DashBoards Menu Start Here------------------------------- */}
+
+
+                {/* --------------Departments----------- */}
+
+
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle sidegood  mt-2"
@@ -106,20 +145,25 @@ export default function Home() {
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
-                                  <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                fill="currentColor"
-                className="bi bi-box-seam-fill m-2"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"
-                />
-              </svg>Department
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="23"
+                      height="23"
+                      fill="currentColor"
+                      className="bi bi-box-seam-fill m-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"
+                      />
+                    </svg>Department
                   </a>
+
+                  {/* ---------------------Departments Dropdown Start Here---------------- */}
+
+
+
                   <ul className="dropdown-menu dropdown-menu-dark">
                     <li>
                       <a className="dropdown-item" href="/">
@@ -136,6 +180,11 @@ export default function Home() {
                     </li>
                   </ul>
                 </li>
+
+                {/* ---------------Staffs----------------  */}
+
+
+
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle sidegood  mt-2"
@@ -145,16 +194,22 @@ export default function Home() {
                     aria-expanded="false"
                   >
                     <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                fill="currentColor"
-                className="bi bi-inboxes-fill m-2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zM3.81.563A1.5 1.5 0 0 1 4.98 0h6.04a1.5 1.5 0 0 1 1.17.563l3.7 4.625a.5.5 0 0 1 .106.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393z" />
-              </svg>Staff
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="23"
+                      height="23"
+                      fill="currentColor"
+                      className="bi bi-inboxes-fill m-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zM3.81.563A1.5 1.5 0 0 1 4.98 0h6.04a1.5 1.5 0 0 1 1.17.563l3.7 4.625a.5.5 0 0 1 .106.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393z" />
+                    </svg>Staff
                   </a>
+
+                  {/* ------Stafss Dropdown Starts Here------------- */}
+
+
+
+
                   <ul className="dropdown-menu dropdown-menu-dark">
                     <li>
                       <a className="dropdown-item" href="/">
@@ -171,6 +226,11 @@ export default function Home() {
                     </li>
                   </ul>
                 </li>
+
+                {/* -------Salary Starts Here--------- */}
+
+
+
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle sidegood  mt-2"
@@ -180,16 +240,20 @@ export default function Home() {
                     aria-expanded="false"
                   >
                     <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                fill="currentColor"
-                className="bi bi-currency-dollar m-2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
-              </svg>Salary
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="23"
+                      height="23"
+                      fill="currentColor"
+                      className="bi bi-currency-dollar m-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
+                    </svg>Salary
                   </a>
+
+
+                  {/* ------Salary Dropdown Start Here-------- */}
+
                   <ul className="dropdown-menu dropdown-menu-dark">
                     <li>
                       <a className="dropdown-item" href="/">
@@ -206,6 +270,11 @@ export default function Home() {
                     </li>
                   </ul>
                 </li>
+
+                {/* -------Leave Start Here---------- */}
+
+
+
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle sidegood  mt-2"
@@ -215,19 +284,23 @@ export default function Home() {
                     aria-expanded="false"
                   >
                     <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="23"
-                height="23"
-                fill="currentColor"
-                className="bi bi-arrow-return-right m-2"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"
-                />
-              </svg>Leave
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="23"
+                      height="23"
+                      fill="currentColor"
+                      className="bi bi-arrow-return-right m-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"
+                      />
+                    </svg>Leave
                   </a>
+
+                  {/* ------Leave Dropdown Starts Here------- */}
+
+
                   <ul className="dropdown-menu dropdown-menu-dark">
                     <li>
                       <a className="dropdown-item" href="/">
